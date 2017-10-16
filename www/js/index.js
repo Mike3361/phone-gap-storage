a840216ee47d20958b68c8aa4237a0375b77f2cd
Mike3361 <308552353@qq.com>
2017-10-16 12:02:40 +0100

    change

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,33 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
+var key = "pen";
+var value = "blue";
+var key = "pencil";
+var value = "black";
 
+apple = {"name": "BOUNTY", "product": "BOUNTY bar 57g"};
+myChocolate = JSON.stringify(apple);
+localStorage.setItem("apple",myChocolate);
+
+
+
+
+
+
+window.localStorage.setItem("pen","blue");
+window.localStorage.setItem("pencil","black");
+window. localStorage.setItem(key, value);
 document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
+
+var value = window.localStorage.getItem(key);
+
+
+alert(myChocolate)
+alert("black")
+
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
